43989e99c491f9454e27a753043a8848bf28acbd
Priya <pvaidya56@gmail.com>
2020-03-13 08:43:04 -0700

    setup server, running on port 5000

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+// const projectsRouter = require('../projects/projects-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    const query = req.query;
+  
+    console.log("query", query);
+  
+    res.status(200).json(query);
+  });
+
+  
+//   server.use("/api/projects", projectsRouter);
+  
+  module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require('./api/server');
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on port ${port}***\n`);
+})
