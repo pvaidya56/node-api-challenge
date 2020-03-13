bb6c5177b658a500cc22f1ff85689c91f6c89a33
Priya <pvaidya56@gmail.com>
2020-03-13 10:01:55 -0700

    CRUD functionality for projects, need to revisit create post as it is giving an error even though its being created

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const Projects = require("../data/helpers/projectModel");
+const Actions = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
@@ -18,31 +19,93 @@ router.get('/', (req,res) => {
 });
 
 //create a project
-router.post('/', (req, res) => {
-    Projects.insert(req.body)
-    .then(response => {
+router.post('/', validateProject, async (req, res) => {
+    try{
+        const project = await Projects.insert(req.body)
         res.status(201).json(response)
-    })
-    .catch(error => {
-        console.log(error)
+    }
+    catch(err) {
         res.status(500).json({error: "Error creating project"})
-    })
+    }
 });
+
+//GET project by id
+    router.get('/:id', async (req,res) => {
+       try {
+           const project = await Projects.get(req.params.id);
+           if(project) {
+               res.status(200).json(project)
+           } else {
+               res.status(400).json({message: "Project with specified ID does not exist"})
+           }
+       }
+       catch (err) {
+           res.status(500).json({error: "Project not found"})
+       }
+    })
+//update a project
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedProject = await Projects.update(req.params.id, req.body)
+
+        if (updatedProject) {
+            res.status(201).json(updatedProject)
+        }
+        else {
+            res.status(404).json({message: "Project not found"})
+        }
+    }
+    catch (err) {
+        res.status(500).json({error: "Project not be updated"})
+    }
+})
+
+//delete a project
+router.delete('/:id', (req, res) => {
+    Projects.remove(req.params.id)
+        .then(response => {
+            console.log(response)
+            if (response) {
+                res.status(200).json({message: "Project deleted successfully"})
+            }
+            else {
+                res.status(404).json({message: `Project not found`}) 
+            }
+        })
+        .catch(response => {
+            res.status(500).json({error: "Project could not be deleted"})
+        })
+})
+
+
 
 
 //custom middleware
-//ensures that a project with specified id exists 
-function validateProjectId(req, res, next) {
-    Projects.getById(req.params.id)
-    .then(project => {
-        if (project.id){
-            req.project = project;
-        } next();
-    })
-    .catch(error => {
-        console.log(error)
-        res.status(400).json({message: "Invalid Project"})
-      })
-}
+
+//ensures that project exists
+function validateProject(req, res, next) {
+    if (!req.body) {
+      res.status(400).json({message: "missing user data"})
+    }
+    else if (!req.body.name) {
+      res.status(400).json({message: "missing required name field"})
+    }
+    else {
+      next();
+    }
+  }
+
+  //ensures that action exists
+  function validateAction(req, res, next) {
+    if (!req.body) {
+      res.status(400).json({message: "Missing action data"})
+    }
+    else if (!req.body.description) {
+      res.status(400).json({message: "Missing required description field"})
+    }
+    else {
+      next();
+    }
+  }
 
 module.exports = router;
